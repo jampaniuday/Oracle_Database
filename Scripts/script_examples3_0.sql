ee7ea485783a8c303e39f484bd77899d5e5450fe
pedroAkiraDanno <pedro-akira@hotmail.com>
2020-05-08 20:59:09 -0300

    add script backup RMAN

--- Scripts/script_examples3_0.sql
+++ Scripts/script_examples3_0.sql
@@ -1,6 +1,6 @@
 
 
----------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------
 #backup INCREMENTAL differencial level 0 
 
 
@@ -11,7 +11,7 @@
 #
 
 #************************************************#
-# NOME      : rman_script2_weekly_full.sh        #
+# NOME      : rman_script2_weekly_increLV0.sh    #
 # OBJETIVO  : Script to run weekly full backup   #
 # CRIACAO   : 07-05-2020                         #
 # VERSAO    : 1.0                                #
@@ -59,8 +59,8 @@
 
 
 
-run{
-        rman target / <<EOF>> ${DIR_LOGS}/rman_script2_weekly_full_${DATE}_${TIME}.log
+
+        rman target / <<EOF>> ${DIR_LOGS}/rman_script2_weekly_increLV0_${DATE}_${TIME}.log
         set echo on;
         ALLOCATE CHANNEL c1 DEVICE TYPE DISK;
         CONFIGURE CONTROLFILE AUTOBACKUP ON;
@@ -70,9 +70,7 @@ run{
         sql 'alter database backup controlfile to trace as /backups/controlfile${DATE}_${TIME}.txt';
 
         CONFIGURE CONTROLFILE AUTOBACKUP FORMAT FOR DEVICE TYPE DISK TO '${DIR_BKP_DAYS}/%F_${DATE}_${TIME}';
-        BACKUP INCREMENTAL LEVEL 0 DATABASE PLUS ARCHIVELOG  FORMAT '${DIR_BKP_DAYS}/XE_full_${DATE}_${TIME}_%U.bck';
-
-}
+        BACKUP INCREMENTAL LEVEL 0 DATABASE PLUS ARCHIVELOG  TAG 'weekly_increLV0_db_bkup' FORMAT '${DIR_BKP_DAYS}/%D_increLV0_${DATE}_${TIME}_%U_%T_%S_%P.bck';
 
 
 
@@ -100,8 +98,8 @@ run{
 
 
 
----------------------------------------------------------------------------------------------------------------------------------
-
+---------------------------------------------------------------------------------------------------------------------------------------------------
+#backup INCREMENTAL differencial level 1
 
 
 
@@ -111,13 +109,13 @@ run{
 #!/bin/bash
 #
 
-#************************************************#
-# NOME      : rman_script2_weekly_full.sh        #
-# OBJETIVO  : Script to run weekly full backup   #
-# CRIACAO   : 07-05-2020                         #
-# VERSAO    : 1.0                                #
-# AUTOR     : Pedro Akira Danno Lima             #
-#************************************************#
+#****************************************************#
+# NOME      : rman_script2_daily_increLV1.sh         #
+# OBJETIVO  : Script to run daily incremental backup #
+# CRIACAO   : 07-05-2020                             #
+# VERSAO    : 1.0                                    #
+# AUTOR     : Pedro Akira Danno Lima                 #
+#****************************************************#
 
 
 #variaveis
@@ -160,8 +158,8 @@ run{
 
 
 
-run{
-        rman target / <<EOF>> ${DIR_LOGS}/rman_script2_weekly_full_${DATE}_${TIME}.log
+
+        rman target / <<EOF>> ${DIR_LOGS}/rman_script2_daily_increLV1.sh${DATE}_${TIME}.log
         set echo on;
         ALLOCATE CHANNEL c1 DEVICE TYPE DISK;
         CONFIGURE CONTROLFILE AUTOBACKUP ON;
@@ -171,9 +169,9 @@ run{
         sql 'alter database backup controlfile to trace as /backups/controlfile${DATE}_${TIME}.txt';
 
         CONFIGURE CONTROLFILE AUTOBACKUP FORMAT FOR DEVICE TYPE DISK TO '${DIR_BKP_DAYS}/%F_${DATE}_${TIME}';
-        BACKUP INCREMENTAL LEVEL 0 DATABASE PLUS ARCHIVELOG  FORMAT '${DIR_BKP_DAYS}/XE_full_${DATE}_${TIME}_%U.bck';
+        BACKUP INCREMENTAL LEVEL 1 DATABASE PLUS ARCHIVELOG  FORMAT '${DIR_BKP_DAYS}/XE_increLV1_${DATE}_${TIME}_%U.bck';
 
-}
+
 
 
 
@@ -201,4 +199,43 @@ run{
 
 
 
----------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Minuto (0 a 59) | Hora (0 a 23) | Dia (1 a 31) | Mês (1 a 12)| Dia da semana (0 a 7) | Comando (comando a ser executado)
+
+#test
+#* 0,4,8,12,16,20 * * * "/home/oracle/scripts/rman_script2_weekly_full.sh" > /dev/null
+
+
+# executar:  00 seg | 23 horas | todos os dias | todos os meses | aos domingos
+0 0 * * 7  "/home/oracle/scripts/rman_script2_weekly_increLV0.sh"
+
+# executar de segunda a sexta, nas horas: 6h, 12h,18h,00h
+0 6,12,18,0 * * 1-6 "/home/oracle/scripts/rman_script2_daily_increLV1.sh"
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+V$DATABASE_BLOCK_CORRUPTION
+
+
+A flash recovery area
+A disk location in which the database can store and manage files related to backup
+and recovery. You set the flash recovery area location and size with the DB_
+RECOVERY_FILE_DEST and DB_RECOVERY_FILE_DEST_SIZE initialization
+parameters.
+
+
+
+ 
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------

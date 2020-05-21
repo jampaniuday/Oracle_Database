b72f1f3254f016cc61977df2616913aea8fb5185
pedroAkiraDanno <pedro-akira@hotmail.com>
2020-05-21 09:10:55 -0300

    add new files .pdf about DBMS_SCHEDULER realise 11g and 12c

---
+++ notes_imgs_diversos/DBMS_SCHEDULER.sql
@@ -0,0 +1,48 @@
+---------------------------------------------------------------------------------------------------------------------------------
+DBMS_SCHEDULER | DBMS_JOB | 11g
+
+DBMS_SCHEDULER.CREATE_JOB | DBMS_SCHEDULER.CREATE_PROGRAM   | DBMS_SCHEDULER.create_schedule| DBMS_SCHEDULER.CREATE_WINDOW | DBMS_SCHEDULER.CREATE_CHAIN 
+
+
+#Estrutura completa da package DBMS_SCHEDULER
+DESC DBMS_SCHEDULER
+
+
+#Estrutura completa da package DBMS_JOB
+DESC DBMS_JOB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+refes: 
+obs:
+desc: 
+
+---------------------------------------------------------------------------------------------------------------------------------
